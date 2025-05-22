c0834d6dd59cb74dc582068595fbc7ce5d2e9c00
Jeremias Pokemayer <jerepoke2016@gmail.com>
2025-05-22 19:54:07 -0300

    update cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -3,7 +3,18 @@ import initMiddleware from "./middlewares";
 
 const cors = initMiddleware(
   Cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://e-commerce-frontend-sigma-ten.vercel.app",
+      ];
+
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Authorization", "Content-Type"],
     credentials: true,
